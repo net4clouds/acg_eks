807b4ec80c5a7fe14e6304663a8da22a8ce9473e
net4clouds <68735686+net4clouds@users.noreply.github.com>
2024-03-04 15:30:19 +0100

    roles2

--- iam-roles-polices-attach.tf
+++ iam-roles-polices-attach.tf
@@ -17,31 +17,31 @@ resource "aws_iam_role_policy_attachment" "eks-cl-role-policy2-attachment" {
 
 resource "aws_iam_role_policy_attachment" "eks-cl-role-policy3-attachment" {
   role       = aws_iam_role.eks-cluster-service.name
-  policy_arn = aws_iam_policy.eks-CloudWatchMetrics.arn
+  policy_arn = data.aws_iam_policy.AmazonEKSServiceRolePolicy.arn
 }
 
-resource "aws_iam_role_policy_attachment" "eks-cl-role-policy4-attachment" {
-  role       = aws_iam_role.eks-cluster-service.name
-  policy_arn = aws_iam_policy.eks-ELB.arn
+resource "aws_iam_role_policy_attachment" "eks-nd-role-policy0-attachment" {
+  role       = aws_iam_role.eks-node.name
+  policy_arn = data.aws_iam_policy.AWSServiceRoleForAmazonEKSNodegroup.arn
 }
 
 resource "aws_iam_role_policy_attachment" "eks-nd-role-policy1-attachment" {
-  role       = aws_iam_role.eks-NodeInstanceRole.name
+  role       = aws_iam_role.eks-node.name
   policy_arn = data.aws_iam_policy.AmazonEC2ContainerRegistryReadOnly.arn
 }
 
 resource "aws_iam_role_policy_attachment" "eks-nd-role-policy2-attachment" {
-  role       = aws_iam_role.eks-NodeInstanceRole.name
+  role       = aws_iam_role.eks-node.name
   policy_arn = data.aws_iam_policy.AmazonEKS_CNI_Policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "eks-nd-role-policy3-attachment" {
-  role       = aws_iam_role.eks-NodeInstanceRole.name
+  role       = aws_iam_role.eks-node.name
   policy_arn = data.aws_iam_policy.AmazonEKSWorkerNodePolicy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "eks-nd-role-policy4-attachment" {
-  role       = aws_iam_role.eks-NodeInstanceRole.name
+  role       = aws_iam_role.eks-node.name
   policy_arn = data.aws_iam_policy.AmazonSSMManagedInstanceCore.arn
 }
 
