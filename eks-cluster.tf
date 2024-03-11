e42adaf802cc9f3879f883a44e7eb0db084b215b
net4clouds <68735686+net4clouds@users.noreply.github.com>
2024-03-11 15:46:22 +0100

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -5,8 +5,9 @@ resource "aws_eks_cluster" "cl01" {
 
   vpc_config {
     subnet_ids = [ aws_subnet.sub_A_in.id, aws_subnet.sub_B_in.id, aws_subnet.sub_C_in.id ]
-	endpoint_private_access = true
+  	endpoint_private_access = true
     endpoint_public_access  = false
+    security_group_ids = [aws_security_group.https-internal.id]
   }
 }
 
