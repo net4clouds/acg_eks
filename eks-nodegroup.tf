b8a5f9cc46e2629c8164aba51555a4a5d452bd47
net4clouds <68735686+net4clouds@users.noreply.github.com>
2024-03-11 15:48:36 +0100

    Update eks-nodegroup.tf

--- eks-nodegroup.tf
+++ eks-nodegroup.tf
@@ -3,6 +3,7 @@ resource "aws_eks_node_group" "ng01" {
   node_group_name = "ng01"
   node_role_arn   = aws_iam_role.eks-node.arn
   subnet_ids      = [ aws_subnet.sub_A_in.id, aws_subnet.sub_B_in.id, aws_subnet.sub_C_in.id ]
+  security_group_ids = [aws_security_group.https-internal.id]
 
   scaling_config {
     desired_size = 1
