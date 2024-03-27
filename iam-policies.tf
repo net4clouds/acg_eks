e9dfc5fc954859656a559e4f48b0c6569baec948
net4clouds <68735686+net4clouds@users.noreply.github.com>
2024-03-27 16:07:44 +0100

    alb_control

--- iam-policies.tf
+++ iam-policies.tf
@@ -19,3 +19,29 @@ data "aws_iam_policy_document" "assume_role_ec2" {
     actions = ["sts:AssumeRole"]
   }
 }
+
+data "aws_iam_policy_document" "assume_role_albcontrol" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks_openid.arn]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringEquals"
+      variable = aws_eks_cluster.cl01.identity.0.oidc.0.issuer":aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = aws_eks_cluster.cl01.identity.0.oidc.0.issuer":sub"
+      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "eks_control_alb" {
+ name = "eks_control_alb"
+ policy = file("control_alb.json")
+}
--- iam-roles.tf
+++ iam-roles.tf
@@ -9,4 +9,8 @@ resource "aws_iam_role" "eks-node" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_ec2.json
 }
 
+resource "aws_iam_role" "eks-control-alb" {
+  name = "eks-control-alb"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_albcontrol.json
+}
 
