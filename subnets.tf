cd6b15154cff03c0e21b10e16ce818759a81a00e
net4clouds <68735686+net4clouds@users.noreply.github.com>
2024-05-08 12:18:52 +0200

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -3,8 +3,8 @@ resource "aws_subnet" "sub_A_in" {
   cidr_block = var.sub_A_in_cidr
   availability_zone = data.aws_availability_zones.azs.names[0]
     tags = {
-    Name = "sub_A_in"
-	kubernetes.io/role/internal-elb = "1"
+    "Name" = "sub_A_in"
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
 resource "aws_subnet" "sub_B_in" {
@@ -12,8 +12,8 @@ resource "aws_subnet" "sub_B_in" {
   cidr_block = var.sub_B_in_cidr
   availability_zone = data.aws_availability_zones.azs.names[1] 
     tags = {
-    Name = "sub_B_in"
-	kubernetes.io/role/internal-elb = "1"
+    "Name" = "sub_B_in"
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
 resource "aws_subnet" "sub_C_in" {
@@ -21,8 +21,8 @@ resource "aws_subnet" "sub_C_in" {
   cidr_block = var.sub_C_in_cidr
   availability_zone = data.aws_availability_zones.azs.names[2] 
     tags = {
-    Name = "sub_C_in"
-	kubernetes.io/role/internal-elb = "1"
+    "Name" = "sub_C_in"
+    "kubernetes.io/role/internal-elb" = "1"
   }
 }
 resource "aws_subnet" "sub_A_pub" {
@@ -30,8 +30,8 @@ resource "aws_subnet" "sub_A_pub" {
   cidr_block = var.sub_A_pub_cidr
   availability_zone = data.aws_availability_zones.azs.names[0] 
     tags = {
-    Name = "sub_A_pub"
-	kubernetes.io/role/elb = "1"
+    "Name" = "sub_A_pub"
+    "kubernetes.io/role/elb" = "1"
   }
 }
 resource "aws_subnet" "sub_B_pub" {
@@ -39,8 +39,8 @@ resource "aws_subnet" "sub_B_pub" {
   cidr_block = var.sub_B_pub_cidr
   availability_zone = data.aws_availability_zones.azs.names[1]
     tags = {
-    Name = "sub_B_pub"
-	kubernetes.io/role/elb = "1"
+    "Name" = "sub_B_pub"
+    "kubernetes.io/role/elb" = "1"
   }
 }
 resource "aws_subnet" "sub_C_pub" {
@@ -48,7 +48,7 @@ resource "aws_subnet" "sub_C_pub" {
   cidr_block = var.sub_C_pub_cidr
   availability_zone = data.aws_availability_zones.azs.names[2] 
     tags = {
-    Name = "sub_C_pub"
-	kubernetes.io/role/elb = "1"
+    "Name" = "sub_C_pub"
+    "kubernetes.io/role/elb" = "1"
   }
 }
